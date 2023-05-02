e5510653991ae653816b1a618e6e460f7cb29d76
MikePio <michelepiopilla@gmail.com>
2023-05-02 19:01:00 +0200

    Created card component

---
+++ src/components/Card.vue
@@ -0,0 +1,121 @@
+<script>
+export default {
+  name: 'Card',
+  data(){
+    return{
+
+    }
+  }
+
+}
+</script>
+
+<template>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+  <div class="mp-card">
+    <img src="public\img\logo.png" class="card-img-top" alt="img">
+    <div class="card-body">
+      <h5 class="card-title text-center pt-3">Card title</h5>
+      <p class="card-text text-center py-3">Some</p>
+    </div>
+  </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+  @use '../scss/main.scss' as *;
+
+  .mp-card{
+    width: calc(100% / 5 - 20px);
+    // width: calc((100% / 5 ) - 10px);
+    
+    // width: calc(100% / 5 );
+    
+    // width: 250px;
+    height: 400px;
+    // border: 1px blue dashed;
+    // margin: 0 10px 40px 10px;
+    // margin: 0 0px 40px 0px;
+    margin-bottom: 40px;
+    img{
+      height: 300px;
+    }
+    .card-body{
+      background-color: $primary-color;
+    }
+  }
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,10 @@
 <script>
+import Card from './Card.vue'
 export default {
   name: 'Main',
+  components: { 
+    Card
+  },
   data(){
     return{
 
@@ -35,106 +39,111 @@ export default {
         <!-- container cards -->
         <!-- <div class="container p-3 my-3 d-flex flex-wrap"> -->
         <div class="container d-flex flex-wrap justify-content-between p-0">
+
+          <Card/>
           
           <!-- <div class="container d-flex flex-wrap p-0"> -->
 
-            <!--* card -->
-<!--             
-          <div class="mp-card mb-3">
+            <!--* card  con mb-3-->
+            <!--             
+              <div class="mp-card mb-3">
+                <img src="public\img\logo.png" class="card-img-top" alt="img">
+                <div class="card-body">
+                  <h5 class="card-title text-center pt-3">Card title</h5>
+                  <p class="card-text text-center py-3">Some</p>
+                </div>
+              </div> -->
+              
+              <!--* card con col-->
+              <!-- <div class="mp-card col">
+                <img src="public\img\logo.png" class="card-img-top" alt="img">
+                <div class="card-body">
+                  <h5 class="card-title text-center pt-3">Card title</h5>
+                  <p class="card-text text-center py-3">Some</p>
+                </div>
+              </div> -->
+              
+              
+              <!--* card senza col -->
+          <!-- <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
+            <img src="public\img\logo.png" class="card-img-top" alt="img">
+            <div class="card-body">
+              <h5 class="card-title text-center pt-3">Card title</h5>
+              <p class="card-text text-center py-3">Some</p>
+            </div>
+          </div>
+            
+          <div class="mp-card">
             <img src="public\img\logo.png" class="card-img-top" alt="img">
             <div class="card-body">
               <h5 class="card-title text-center pt-3">Card title</h5>
               <p class="card-text text-center py-3">Some</p>
             </div>
           </div> -->
-            
-          <!-- <div class="mp-card col">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div> -->
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
-            
-          <div class="mp-card">
-            <img src="public\img\logo.png" class="card-img-top" alt="img">
-            <div class="card-body">
-              <h5 class="card-title text-center pt-3">Card title</h5>
-              <p class="card-text text-center py-3">Some</p>
-            </div>
-          </div>
             
 
 
@@ -174,25 +183,28 @@ main{
     }
     
   }
-  .mp-card{
-    width: calc(100% / 5 - 20px);
-    // width: calc((100% / 5 ) - 10px);
+
+  //CODICE UTILIZZATO IN PRECEDENZA PER LE CARDS E ADESSO SPOSTATO NEL COMPONENTE CARD 
+
+  // .mp-card{
+  //   width: calc(100% / 5 - 20px);
+  //   // width: calc((100% / 5 ) - 10px);
     
-    // width: calc(100% / 5 );
+  //   // width: calc(100% / 5 );
     
-    // width: 250px;
-    height: 400px;
-    // border: 1px blue dashed;
-    // margin: 0 10px 40px 10px;
-    // margin: 0 0px 40px 0px;
-    margin-bottom: 40px;
-    img{
-      height: 300px;
-    }
-    .card-body{
-      background-color: $primary-color;
-    }
-  }
+  //   // width: 250px;
+  //   height: 400px;
+  //   // border: 1px blue dashed;
+  //   // margin: 0 10px 40px 10px;
+  //   // margin: 0 0px 40px 0px;
+  //   margin-bottom: 40px;
+  //   img{
+  //     height: 300px;
+  //   }
+  //   .card-body{
+  //     background-color: $primary-color;
+  //   }
+  // }
 
 }
 </style>
